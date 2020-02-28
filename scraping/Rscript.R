56b455fff8a4b0dc7bc4710875e2db7cfccb017c
Hans H. Sievertsen <hhsievertsen@users.noreply.github.com>
2020-02-28 17:48:19 +0000

    corrected version

--- scraping/Rscript.R
+++ scraping/Rscript.R
@@ -1,26 +1,52 @@
 setwd ("C:\\Github\\igm_survey_gender\\scraping")
 library("tidyverse")
+library(stringr)
 
 # load data
-df<-read_delim("data_output.csv",delim="|")
+df<-read_delim("data_output_US.csv",delim="|")
 # Confidence
 df1<-df%>%select(Confidence,Name)%>%
-  separate(Confidence,into = paste("CONF", 1:119, sep = "_"),sep=";")%>%
-  pivot_longer(1:119, names_to="Question",values_to = "Confidence")%>%
+  separate(Confidence,into = paste("CONF", 1:1000, sep = "_"),sep=";")%>%
+  pivot_longer(1:1000, names_to="Question",values_to = "Confidence")%>%
   mutate(Confidence=as.numeric(Confidence))
 # Medianconf
 df2<-df%>%select(Medianconf,Name)%>%
-  separate(Medianconf,into = paste("CONF", 1:119, sep = "_"),sep=";")%>%
-  pivot_longer(1:119, names_to="Question",values_to = "Medianconf")%>%
+  separate(Medianconf,into = paste("CONF", 1:1000, sep = "_"),sep=";")%>%
+  pivot_longer(1:1000, names_to="Question",values_to = "Medianconf")%>%
   mutate(Medianconf=as.numeric(Medianconf))
-df3<-df%>%select(Questions,Name)%>%
-  separate(Questions,into = paste("CONF", 1:119, sep = "_"),sep=";")%>%
-  pivot_longer(1:119, names_to="Question",values_to = "Questiontext")
+# Vote
+df3<-df%>%select(Votes,Name)%>%
+  separate(Votes,into = paste("CONF", 1:1000, sep = "_"),sep=";")%>%
+  pivot_longer(1:1000, names_to="Question",values_to = "Vote")
+# Median Vote
+df4<-df%>%select(Median,Name)%>%
+  separate(Median,into = paste("CONF", 1:1000, sep = "_"),sep=";")%>%
+  pivot_longer(1:1000, names_to="Question",values_to = "Median")
+# Comment 
+df5<-df%>%select(Comment,Name)%>%
+  separate(Comment,into = paste("CONF", 1:1000, sep = "_"),sep=";")%>%
+  pivot_longer(1:1000, names_to="Question",values_to = "Comment")
+# Institution 
+df6<-df%>%select(Profile,Name)%>%
+  mutate(Profile=str_remove(Profile, Name),
+         Profile=str_remove(Profile, "Personal Homepage"),
+        Profile=str_trim(Profile))%>%
+  rename(Institution=Profile)
+
 # merge
-df4<-merge(df1,df2,by=c("Name","Question"))
-df4<-merge(df4,df3,by=c("Name","Question"))%>%
-      mutate(Question=as.numeric(substr(Question,6,6)),
-             Deviation_from_median_confidence=Confidence-Medianconf)
-write_csv(df4,"cleaned_data_not_all_elements.csv")
+df_m<-merge(df6,df1,by=c("Name"))
+df_m<-merge(df_m,df2,by=c("Name","Question"))
+df_m<-merge(df_m,df3,by=c("Name","Question"))
+df_m<-merge(df_m,df4,by=c("Name","Question"))
+df_m<-merge(df_m,df5,by=c("Name","Question"))%>%
+      mutate(Question=as.numeric(str_remove(Question, "CONF_")))%>%
+      arrange(Name,Question)%>%
+      filter(!is.na(Medianconf))
+# write csv
+write_excel_csv(df_m,"cleaned_data_US.csv")
+write_excel_csv(df6%>%select(Name,Institution),"cleaned_data_namesonly_US.csv")
+
+
+
 # Plot
-ggplot(df4,aes(x=Deviation_from_median_confidence))+geom_bar()
+#ggplot(df4,aes(x=Deviation_from_median_confidence))+geom_bar()
